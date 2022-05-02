38a5206f3634c936a3deed8432cc11bd09ef482c
Helton Bustos <heltonsmith@hotmail.com>
2022-05-02 00:09:37 -0400

    si

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Supermercado
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.Initialize_on_precompile = false
   end
 end
